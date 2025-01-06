4356bfaa10bd0896e923f17dc8e311e94762e519
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-06 11:24:23 +0000

    24

--- megaBlog-appwrite/src/components/index.js
+++ megaBlog-appwrite/src/components/index.js
@@ -2,11 +2,15 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
-import LogoutBtn from "./header/LogoutBtn"
+import LogoutBtn from "./header/LogoutBtn";
+import Select from './Select'
+import PostCard from "./PostCard";
 export{
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Select,
+    PostCard
 }
