d0e8ae9934d71afa22b554c3945cd95de60602b8
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-08 05:28:57 +0000

    DEbugging left

--- megaBlog-appwrite/src/components/index.js
+++ megaBlog-appwrite/src/components/index.js
@@ -6,13 +6,21 @@ import LogoutBtn from "./header/LogoutBtn";
 import Select from './Select'
 import PostCard from "./PostCard";
 import RTE from './RTE'
+import SignUp from './SignUp'
+import Login from "./Login";
+import Postform from './postform/Postform'
+import AuthLayout from './AuthLayout'
 export{
     Header,
+    AuthLayout,
     Footer,
     Logo,
     Container,
     LogoutBtn,
     Select,
+    Postform,
     PostCard,
+    Login,
+    SignUp,
     RTE
 }
