f15f825e49a0ca30e0b35f9bf4d28afb53746c83
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-15 09:34:32 +0000

    coin

--- crypto-currency tracker/src/components/pages/coin/Coin.jsx
+++ crypto-currency tracker/src/components/pages/coin/Coin.jsx
@@ -1,35 +1,49 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CoinContext } from '../../context/CoinContext';
+import LineChart from '../../LineChart/LineChart';
 
 const Coin = () => {
-  const [coinData, setCoinData] = useState(null); 
+  const [coinData, setCoinData] = useState(null);
+  const [historicalData, setHistoricalData] = useState(null);
   const { coinId } = useParams();
   const { currency } = useContext(CoinContext);
 
   const fetchCoinData = async () => {
     try {
       const res = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`);
+      if (!res.ok) throw new Error('Failed to fetch coin data');
       const data = await res.json();
       setCoinData(data);
     } catch (error) {
-      console.error("Error fetching coin data:", error);
+      console.error('Error fetching coin data:', error);
+    }
+  };
+
+  const fetchHistoricalData = async () => {
+    try {
+      const res = await fetch(
+        `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency}`
+      );
+      if (!res.ok) throw new Error('Failed to fetch historical data');
+      const data = await res.json();
+      setHistoricalData(data);
+    } catch (error) {
+      console.error('Error fetching historical coin data:', error);
     }
   };
 
   useEffect(() => {
     fetchCoinData();
+    fetchHistoricalData();
   }, [currency]);
 
-  if (!coinData) {
-
+  if (!coinData || !historicalData) {
     return (
-
-      <div className='grid place-self-center min-h-[80vh]'>
-        <div className='animate-rotate w-[65px] h-[65px] place-self-center border-[5px] border-solid border-[#bdbdbd] border-t-[#4500c6] rounded-full'></div>
+      <div className="grid place-items-center min-h-[80vh]">
+        <div className="animate-spin w-[65px] h-[65px] border-[5px] border-solid border-[#bdbdbd] border-t-[#4500c6] rounded-full"></div>
       </div>
-  
-  );
+    );
   }
 
   return (
@@ -39,6 +53,9 @@ const Coin = () => {
         <p className="font-bold">
           {coinData.name} ({coinData.symbol.toUpperCase()})
         </p>
+      </div>
+      <div>
+        <LineChart historicalData={historicalData} />
       </div>
     </div>
   );
