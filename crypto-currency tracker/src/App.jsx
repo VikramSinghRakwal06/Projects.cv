6fdb8c1e0edcdbcbfbfde04434f1a3a768a3e861
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-16 20:48:40 +0000

    done

--- crypto-currency tracker/src/App.jsx
+++ crypto-currency tracker/src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import NavBar from './components/Nav-bar/NavBar'
-import Home from './components/pages/home/Home'
-import Footer from './components/footer/Footer'
-import Coin from './components/pages/coin/Coin'
-import {  Route, Routes } from 'react-router-dom'
-function App(){
- 
-  
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Nav-bar/NavBar';
+import Home from './components/pages/home/Home';
+import Footer from './components/footer/Footer';
+import Coin from './components/pages/coin/Coin';
+
+function App() {
   return (
-    <div className='bg-gradient-to-r from-color-one via-color-two to-color-three text-white min-h-screen font-s'>
-      <NavBar/>
-     
-      <Routes>
-      <Route path='/' element={<Home/>}/>,
-      <Route path='/coin/:coinId' element={<Coin/>}/>
-      </Routes>
-      <Footer/>
-    </div>
-  )
+  
+      <div className="bg-gradient-to-r from-color-one via-color-two to-color-three text-white min-h-screen font-s">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coin/:coinId" element={<Coin />} />
+        </Routes>
+        <Footer />
+      </div>
+   
+  );
 }
 
-export default App
+export default App;
